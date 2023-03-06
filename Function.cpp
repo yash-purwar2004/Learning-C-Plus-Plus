4805831c4615a251c52b5310ec9fac5899675418
Yash Purwar <nayan251198@gmail.com>
2023-03-06 12:30:11 +0530

    Add a two numbers of different datatypes by using function template

--- Function.cpp
+++ Function.cpp
@@ -1,21 +1,49 @@
+// #include<iostream>
+// using namespace std;
+
+// void display(){ // Make a Function
+//     cout<<"This is a function"<<endl;
+// }
+
+// int add(int x, int y){ // Function for adding a two numbers
+//     int z;
+//     z = x+y;
+//     return z;
+// }
+
+// int add(int x, int y, int z){ // Function Overloading
+//     int w;
+//     w = x+y+z;
+//     return w;
+// }
+
+// int main(){
+//     display();
+//     int a=10, b=10, c, d;
+//     c = add(a,b);
+//     cout<<"Some else "<<c<<endl;
+
+//     d = add(a,b,c);
+//     cout<<d<<endl;
+// }
+
 #include<iostream>
 using namespace std;
-
-void display(){ // Make a Function
-    cout<<"This is a function"<<endl;
+    
+template<class T> // Function Template
+T maxim(T a,T b)
+    {
+       return a+b;
+    }
+    
+int main()
+{
+	cout<<maxim(12,14)<<endl;
+	cout<<maxim(2.3,1.4)<<endl;
+	cout<<maxim(2.3f,5.6f)<<endl;
+	
+	return 0;
+	    
 }
-
-int add(int x, int y){ // Function for adding a two numbers
-    int z;
-    z = x+y;
-    return z;
-}
-
-int main(){
-    display();
-    int a=10, b=10, c;
-    c = add(a,b);
-    cout<<"Some else "<<c<<endl;
-}
-
-
+    
+	 
