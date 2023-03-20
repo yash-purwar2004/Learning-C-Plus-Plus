4bf966a79ef53988a2f1528d150bb4080a9ffd6d
Yash Purwar <nayan251198@gmail.com>
2023-03-20 09:52:30 +0530

    Define a class by Dynamic Allocation method

--- OOPS_CONCEPT.cpp
+++ OOPS_CONCEPT.cpp
@@ -1,29 +1,61 @@
+// #include<iostream>
+// using namespace std;
+
+// class Hero{
+//     public:
+//     int health;
+
+//     private:
+//     char level;
+
+//     public:
+//     char getlevel(){
+//         return level;
+//     }
+
+//     void setlevel(char ch){
+//         level = ch;
+//     }
+// };
+
+// int main(){
+//     Hero h1;
+//     cout<<"Size:"<<sizeof(h1)<<endl;
+//     h1.health = 70;
+//     cout<<"Health is "<<h1.health<<endl;
+
+
+//     h1.setlevel('A'); // Use of getter and setter for accessing private data members
+//     cout<<"Level is "<<h1.getlevel()<<endl;
+// }
+
+
+
+
+// Dynamically Allocation of Class
+
 #include<iostream>
 using namespace std;
 
 class Hero{
-    public:
+private:
     int health;
 
-    private:
+public:
     char level;
 
-    public:
-    char getlevel(){
-        return level;
+    int gethealth(){
+        return health;
     }
 
-    void setlevel(char ch){
-        level = ch;
+    void sethealth(int h){
+        health = h;
     }
 };
 
 int main(){
-    Hero h1;
-    cout<<"Size:"<<sizeof(h1)<<endl;
-    h1.health = 70;
-    cout<<"Health is "<<h1.health<<endl;
+    Hero *b = new Hero;
 
-    h1.setlevel('A');
-    cout<<"Level is "<<h1.getlevel()<<endl;
+    (*b).sethealth(30);
+    cout<<"Health is "<<(*b).gethealth()<<endl;
 }
