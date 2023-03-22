befbf528ebb9c95b6c49aad029f8718596fc0d09
Yash Purwar <nayan251198@gmail.com>
2023-03-22 23:53:41 +0530

    Code of Parametrized constructor and Copy Constructor

--- OOPS_CONCEPT.cpp
+++ OOPS_CONCEPT.cpp
@@ -34,28 +34,162 @@
 
 // Dynamically Allocation of Class
 
+// #include<iostream>
+// using namespace std;
+
+// class yash{
+// private:
+//     int health;
+
+// public:
+//     char level;
+
+//     int gethealth(){
+//         return health;
+//     }
+
+//     void sethealth(int h){
+//         health = h;
+//     }
+// };
+
+// int main(){
+//     yash *b = new yash;
+
+//     b->sethealth(30);
+//     cout<<"Health is "<<b->gethealth()<<endl;
+// }
+
+
+
+
+
+
+
+// // Constructor
+// #include<iostream>
+// using namespace std;
+//  class hero{
+//     public:
+//     hero(){
+//         cout<<"Constructor Called"<<endl;
+//     }
+//  };
+
+// int main(){
+//     hero ramesh; //Stattically
+
+//     hero *h = new hero; // Dynamically
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Parametrized Constructor
+// #include<iostream>
+// using namespace std;
+// class hero{
+
+// public:
+// int health; //ramesh health
+
+// hero(int health){ // Function input health
+    
+//     this -> health = health;
+//     cout<<"this is "<<this<<endl;
+// }
+// };
+
+// int main(){
+//     hero ramesh(12);
+//     cout<<"address of ramesh"<<&ramesh<<endl;
+// }
+
+
+
+
+
+
+
+
+
+// Copy Constructor
+// #include<iostream>
+// using namespace std;
+
+//     class hero{
+//         public:
+//         hero(){
+//             cout<<"Default Constructor called"<<endl;
+//         }
+
+//         int health;
+//     };
+
+
+
+// int main(){
+//     hero suresh;
+//     suresh.health = 70;
+//     cout<<"Health is "<<suresh.health<<endl;
+
+//     hero R(suresh); // Copy Constructor Called
+//     cout<<"Health is "<<R.health<<endl;
+// }
+
+
+
+
+// Copy Constructor
 #include<iostream>
 using namespace std;
 
-class Hero{
-private:
-    int health;
+class hero{
+    public:
+        int health;
+        char level;
 
-public:
-    char level;
+        hero(int x, char y){
+            health = x;
+            level = y;
+        }
 
-    int gethealth(){
-        return health;
-    }
+        int gethealth(){
+            return health;
+        }
 
-    void sethealth(int h){
-        health = h;
-    }
+        char getlevel(){
+            return level;
+        }
 };
 
 int main(){
-    Hero *b = new Hero;
+    hero ramesh(20,'B');
+    hero suresh(50,'H');
 
-    (*b).sethealth(30);
-    cout<<"Health is "<<(*b).gethealth()<<endl;
+    ramesh = suresh;
+
+    cout<<"Health is "<<ramesh.health<<endl;
+
+
+
+
 }
+
+
+
+
+
+
+
